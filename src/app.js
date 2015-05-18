3370699819b2a7e5283d07039cab9c625dde7f0c
cdomingo <carl.m.domingo@gmail.com>
2015-05-17 23:33:29 -0400

    Fix initial join

--- src/app.js
+++ src/app.js
@@ -15,7 +15,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var secrets = require('./secrets.js');
-//console.log(secrets["FB_SECRET"]);
 
 // app setup
 app.use('/assets', express.static(path.resolve(__dirname) + '../../client/assets'));
@@ -29,32 +28,21 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser());
 
-// Facebook Login
-passport.use(new FacebookStrategy({
-    clientID: process.env.API_KEY_FACEBOOK_ID || secrets["FB_APP_ID"],
-    clientSecret: process.env.API_KEY_FACEBOOK_SECRET || secrets["FB_SECRET"],
-    callbackURL: 'https://127.0.0.1:'+port+'/facebook-token'
-  },
-  function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function () {
-      return done(null, profile);
-    });
-  }
-));
-
-app.get('/facebook-login', passport.authenticate('facebook'));
- 
-app.get('/facebook-token', passport.authenticate('facebook', { failureRedirect: '/error' }),
-  function(req, res){
-    res.send('Logged In.');
-  });
-
 // Chat
 var port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // listen for a connection
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on("join", function(data) {
+		socket.name = data.name;
+		
+		socket.join('#general');
+		
+		socket.broadcast.to('#general').emit('joinMsg', { name: 'server', msg: data.name + " has joined the room."} );
+		
+		socket.emit('joinMsg', {name: 'server', msg: 'You joined the room'});
+	});
   socket.join('#general');
   // notify server of disconnect
   socket.on('disconnect', function(){
